6332c9d2d0e4a8c86b975000ba473303032ca0f9
Mikołaj Nadzieja <mnadzieja21@gmail.com>
2019-11-20 15:57:16 +0100

    Added missing files

---
+++ 2MDContact/Migrations/201911192327258_Contacts.cs
@@ -0,0 +1,16 @@
+namespace MD2Contact.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Contacts : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
