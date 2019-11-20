5e6bd6c14ba7f7c28b3ab68248fad1da418572d5
Mikołaj Nadzieja <mnadzieja21@gmail.com>
2019-11-20 12:40:59 +0100

    Updated README.md

---
+++ 2MDContact/Models/ContactForms.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MD2Contact.Models
+{
+    public class ContactForms
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AreaOfInterest { get; set; }
+        public string Message { get; set; }
+    }
+}
